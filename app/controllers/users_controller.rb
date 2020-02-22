e6e4010ed20f9839810621560dba963f4ba5fc3e
Dai-yk9123 <yda1r17@gmail.com>
2020-02-22 17:37:23 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # 有効化されていないユーザーの場合、root_urlへ遷移する。※演習
     redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -59,15 +61,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                                   :password_confirmation)
-    end
-
-    # ログイン済みユーザーかどうか確認する
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     # 正しいユーザーかどうか確認する
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   # attr_accessor => :xxというrails内部で使うデータ？オブジェクト属性を作る。
   before_save :downcase_email
@@ -73,6 +74,12 @@ class User < ApplicationRecord
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # 試作feedの定義
+  # 完全な実装は14章にて
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
